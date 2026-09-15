0ee6dc0e8bf42de6d6aec7a0320df75e7fb62124
Alexander Batashev <alex@batashev.me>
2026-09-15 10:28:24 +0300

    refactor: defer shared ISel arithmetic pending native performance fixes

--- utils/symbolic/src/sem/discover.rs
+++ utils/symbolic/src/sem/discover.rs
@@ -239,7 +239,6 @@ impl SmtOracle {
             ));
         }
         match blast(&g, &widths) {
-            Ok(_) if structurally_equal(lhs, rhs) => ProofOutcome::Proven,
             Ok(b) if defined_refinement => proof_outcome(b.solve_defined_equivalence(l, r)),
             Ok(b) => proof_outcome(b.solve()),
             Err(error) => {
