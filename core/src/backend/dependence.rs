9c10f21dc09dfe2a5cd81f554b29091eb6e3f965
Alexander Batashev <alex@batashev.me>
2026-09-16 17:06:41 +0300

    fix(backend): avoid self-dependencies on parameter updates

--- core/src/backend/dependence.rs
+++ core/src/backend/dependence.rs
@@ -293,7 +293,9 @@ fn parameter_edges(context: &Context, ops: &[OpId]) -> Vec<Vec<usize>> {
                 predecessors[index].push(writer);
             }
             for reader in read.remove(&value).unwrap_or_default() {
-                predecessors[index].push(reader);
+                if reader != index {
+                    predecessors[index].push(reader);
+                }
             }
             written.insert(value, index);
         }
